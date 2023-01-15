bbca62c599c74ea4ce8a12f089183d647e7378c3
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-15 12:56:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import './css/styles.css';
 import { fetchImages } from './fetchImages'
-// import Debounce from "lodash.debounce";
 import Notiflix from 'notiflix';
-
-// const DEBOUNCE_DELAY = 300;
 
 let search = '';
 let page = 1;
 
-
 const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
 const guard = document.querySelector(".js-guard");
-
 
 const options = {
     root: null,
@@ -23,7 +18,6 @@ const options = {
 const observe = new IntersectionObserver(onAddImages, options)
 
 form.addEventListener("submit", onSubmit);
-// form.addEventListener("input", new Debounce(onInput, DEBOUNCE_DELAY));
 
 function onSubmit(evt) {
     evt.preventDefault();
@@ -82,90 +76,3 @@ function onAddImages(entries, observe) {
         }
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const DEBOUNCE_DELAY = 300;
-
-// const input = document.querySelector('#search-box');
-// const country = document.querySelector(".country-list");
-// const countryInfo = document.querySelector(".country-info");
-
-// input.addEventListener('input', new Debounce(onInput, DEBOUNCE_DELAY));
-
-// function onInput(evt) {
-//     clearMarkup();
-
-//     if (evt.target.value.trim()) {
-//         fetchCountries(evt.target.value.trim()).then(data => addList(data)).catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'))
-//     }
-// }
-
-// function addList(countryList) {
-//     if (countryList.length > 10) {
-//         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-//     } else if (countryList.length > 1) {
-//         country.innerHTML = createListCountry(countryList);
-//     } else {
-//         countryInfo.innerHTML = createInfoCountry(countryList);
-//     }
-// }
-
-// function clearMarkup() {
-//     country.innerHTML = '';
-//     countryInfo.innerHTML = '';
-// }
-
-// function createListCountry(countryList) {
-//     return countryList.map(({ flags, name }) => `<li class="country-item"><img src="${flags.svg}" alt="" class="country-img" width="25px">${name}</li>`).join('');
-// }
-
-// function createInfoCountry(countryList) {
-//     return countryList.map(({ capital, population, languages, flags, name }) =>
-//     `<div class="country-item info"><img src="${flags.svg}" alt="" class="country-img" width="25px">${name}</div>
-//     <div class="country-item"><span class="country-item_text">Capital: </span> ${capital}</div>
-//     <div class="country-item"><span class="country-item_text">Population: </span> ${population}</div>
-//     <div class="country-item"><span class="country-item_text">Languages: </span> ${languages.map(lan => lan.name)}</div>`).join('');
-// }
